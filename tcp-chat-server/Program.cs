800f8639aad99a6c196f808e40142c568c42a6ed
Aleš Jiránek <a.jiranek@gmail.com>
2015-04-24 13:21:04 +0200

    First connection

--- tcp-chat-server/Program.cs
+++ tcp-chat-server/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace tcp_chat_server
 {
@@ -10,6 +12,21 @@ namespace tcp_chat_server
     {
         static void Main(string[] args)
         {
+            String ip   = "127.0.0.1";
+            int port    = 7777;
+
+            IPAddress ipAddress = IPAddress.Parse(ip);
+
+            Server server = new Server(ipAddress, port);
+
+            if (!server.Start()) {
+                Console.WriteLine("Server cannot be started.");
+                return;
+            }
+
+            Console.WriteLine("Server is listening on port " + port);
+            server.Listen();
+           
         }
     }
 }
---
+++ tcp-chat-server/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tcp_chat_server
+{
+    class Server
+    {
+        private IPAddress   ipAdress;
+        private int         port;
+        private Boolean     isRunning = false;
+        private TcpListener serverSocket;
+        private Hashtable   connectedClients;
+
+        /**
+         * Constructor
+         * 
+         * @param IPAdress ip - address, where server listens for connections
+         */
+        public Server(IPAddress ip, int port) {
+            this.ipAdress = ip;
+            this.port = port;
+            this.connectedClients = new Hashtable();
+        }
+
+        public bool Start() {
+            this.serverSocket = new TcpListener(this.ipAdress, this.port);
+
+            try {
+                this.serverSocket.Start();
+                this.isRunning = true;
+            }
+            catch (SocketException e) {
+                this.isRunning = false;
+                Console.WriteLine("Unable initialize server socket");
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            return isRunning;
+        }
+
+        public void Listen() {
+            while(this.isRunning){
+                TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                this.connectedClients.Add(clientSocket.GetHashCode(),clientSocket);
+                Console.WriteLine("New Connection!");
+            }
+        }
+    }
+}
